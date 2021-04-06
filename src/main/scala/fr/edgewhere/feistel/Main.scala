ed4b0918c96b0f8ba4e92734d4be832e097f0b04
Cyril Dever <cdever@edgewhere.fr>
2021-04-06 10:03:37 +0200

    config to parse command-line

--- src/main/scala/fr/edgewhere/feistel/Main.scala
+++ src/main/scala/fr/edgewhere/feistel/Main.scala
@@ -1,27 +1,28 @@
 package fr.edgewhere.feistel
 
+import fr.edgewhere.feistel.client._
+
 /**
  * Main application entry point
  *
  * @author  Cyril Dever
  * @since   1.0
  * @version 1.0
- *
  * @example {{{
- *  $ sbt assembly && java -cp target/scala-2.12/feistel-jar-0.1.0.jar fr.edgewhere.feistel.Main -i=myWordToObfuscate -e
+ *  $ sbt assembly && java -cp target/scala-2.12/feistel-jar-0.1.0.jar fr.edgewhere.feistel.Main -i=myWordToObfuscate
  *  $ java -cp target/scala-2.12/feistel-jar-0.1.0.jar fr.edgewhere.feistel.Main -i=myObfuscatedWord -d
  * }}}
  */
 object Main extends App {
-  /*try {
+  try {
     // Prepare operation
     val config = Config.init(args = args)
     if (!config.checks) throw new Exception("Bad arguments: verify usage")
-    val operation = if (config.expand) Operation.EXPANSION else Operation.REDACTION
+    val operation = if (config.decrypt) Operation.DECRYPT else Operation.ENCRYPT
 
     // Process operation
-    val worker = Redacted(operation, config)
-    if (!worker.process) throw new Exception("Unable to process operation")
+//    val worker = Redacted(operation, config)
+//    if (!worker.process) throw new Exception("Unable to process operation")
   } catch {
     case e: Exception =>
       println(e)
@@ -29,5 +30,5 @@ object Main extends App {
       println
       println(Config.getUsage)
       System.exit(1)
-  }*/
+  }
 }
