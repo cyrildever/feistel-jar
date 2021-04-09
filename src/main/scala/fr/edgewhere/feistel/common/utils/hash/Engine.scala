b42deea4f4265d92d8c3795fddb3c9447dd3d2e3
Cyril Dever <cdever@edgewhere.fr>
2021-04-09 11:00:10 +0200

    sha3 with secure bouncy castle provided

--- src/main/scala/fr/edgewhere/feistel/common/utils/hash/Engine.scala
+++ src/main/scala/fr/edgewhere/feistel/common/utils/hash/Engine.scala
@@ -1,6 +1,7 @@
 package fr.edgewhere.feistel.common.utils.hash
 
 import fr.edgewhere.feistel.common.exception.UnknownEngineException
+import java.security.Security
 import org.bouncycastle.jcajce.provider.digest.SHA3._
 import scorex.crypto.hash._
 
@@ -9,10 +10,11 @@ import scorex.crypto.hash._
  *
  * @author  Cyril Dever
  * @since   1.0
- * @version 1.0
+ * @version 1.1
  */
 object Engine {
   import Hash._
+  Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider())
 
   type Engine = String
 
@@ -22,7 +24,7 @@ object Engine {
   val SHA_3: Engine = "sha3-256"
 
   def isAvailable(engine: String): Boolean =
-    engine == BLAKE2b || engine == KECCAK || engine == SHA_256
+    engine == BLAKE2b || engine == KECCAK || engine == SHA_256 || engine == SHA_3 // TODO Enrich at each new engine
 
   def hash(input: String, using: Engine): Hash = using match {
     case BLAKE2b =>
