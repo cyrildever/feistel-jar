a949c160ed0af34870de5a331665ccf53f66dd12
Cyril Dever <cdever@edgewhere.fr>
2021-12-14 11:42:55 +0100

    remove javax.xml.bind.DatatypeConverter from dependencies
    - add hex utilities
    - tests

--- build.sbt
+++ build.sbt
@@ -1,6 +1,6 @@
 organization := "fr.edgewhere"
 name := "feistel-jar"
-version := "1.3.5"
+version := "1.4.0"
 scalaVersion := "2.12.13"
 
 assemblyMergeStrategy in assembly := {
--- src/main/scala/fr/edgewhere/feistel/common/utils/hash/Hash.scala
+++ src/main/scala/fr/edgewhere/feistel/common/utils/hash/Hash.scala
@@ -1,20 +1,20 @@
 package fr.edgewhere.feistel.common.utils.hash
 
-import javax.xml.bind.DatatypeConverter
+import fr.edgewhere.feistel.common.utils.hex.Hex
 
 /**
  * Hash type
  *
  * @author  Cyril Dever
- * @since   1.0
+ * @since   2.0
  * @version 1.0
  */
 object Hash {
   type Hash = Seq[Byte]
 
   implicit class HashOps(h: Hash) {
-    def toHex: String = DatatypeConverter.printHexBinary(h.toArray).toLowerCase
+    def toHex: String = h.map(b => f"$b%02x").mkString.toLowerCase
   }
 
-  def fromHex(str: String): Hash = DatatypeConverter.parseHexBinary(str)
+  def fromHex(str: String): Hash = Hex.stringToSomeByteArray(str).getOrElse(Seq.empty)
 }
